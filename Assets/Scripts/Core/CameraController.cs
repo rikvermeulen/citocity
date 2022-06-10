50405ba2473a967086b53f8d005443992457a2af
koenbenne <koen.benne@gmail.com>
2022-06-10 14:14:47 +0200

    small camera movement fix

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Should scale from mouse position to coordinates
-    private int dragSpeed = 35;
+    // Should scale from mouse position to coordinates.
+    // There is definetely a far better way to get these factors instead of random numbers that happen to work.
+    // It's probably based on the width and height of the canvas.
+    private int xFactor = 35;
+    private int yFactor = 18;
     private Vector3 dragOrigin;
 
     void Update()
@@ -17,10 +20,10 @@ public class CameraController : MonoBehaviour
         if (!Input.GetMouseButton(0)) return;
 
         Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
-        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-
-        dragOrigin = Input.mousePosition;
+        Vector3 move = new Vector3(pos.x * xFactor, pos.y * yFactor);
 
         transform.Translate(move, Space.World);
+
+        dragOrigin = Input.mousePosition;
     }
 }
