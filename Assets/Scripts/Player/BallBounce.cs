b71d3a7a0fb65854f0fc15e73ca61972812ec652
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2022-06-16 14:24:22 +0200

    removed old script

--- Assets/Scripts/Player/BallBounce.cs
+++ Assets/Scripts/Player/BallBounce.cs
@@ -8,22 +8,22 @@ public class BallBounce : MonoBehaviour
     Vector3 lastVelocity;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    // void Start()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
 
-    }
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        lastVelocity = rb.lastVelocity;
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     lastVelocity = rb.lastVelocity;
+    // }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        var speed = lastVelocity.magnitude;
-        var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
-        rb.velocity = direction  * Mathf.Max(speed, 0f);
-    }
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     var speed = lastVelocity.magnitude;
+    //     var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+    //     rb.velocity = direction  * Mathf.Max(speed, 0f);
+    // }
 }
