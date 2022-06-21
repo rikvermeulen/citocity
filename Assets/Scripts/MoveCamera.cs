26f44add171d08b7b5cb774b469ceed1ae2991c6
Esolutions-IT <43686864+Esolutions-IT@users.noreply.github.com>
2022-06-21 04:25:59 -0700

    Extra files
    Extra files

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -8,6 +8,13 @@ public class MoveCamera : MonoBehaviour
     public GameObject cam2;
     public GameObject cam3;
     public GameObject cam4;
+    public GameObject cam5;
+    public GameObject cam6;
+    public GameObject cam7;
+    public GameObject cam8;
+    public GameObject cam9;
+    public GameObject cam10;
+    
 
     // Update is called once per frame
     void Update()
@@ -17,6 +24,12 @@ public class MoveCamera : MonoBehaviour
             cam2.SetActive(false);
             cam3.SetActive(false);
             cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
         }
 
         if(Input.GetButtonDown("2Key")){
@@ -24,6 +37,12 @@ public class MoveCamera : MonoBehaviour
             cam2.SetActive(true);
             cam3.SetActive(false);
             cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
         }
 
         if(Input.GetButtonDown("3Key")){
@@ -31,6 +50,12 @@ public class MoveCamera : MonoBehaviour
             cam2.SetActive(false);
             cam3.SetActive(true);
             cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
         }
 
         if(Input.GetButtonDown("4Key")){
@@ -38,6 +63,90 @@ public class MoveCamera : MonoBehaviour
             cam2.SetActive(false);
             cam3.SetActive(false);
             cam4.SetActive(true);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("5Key")){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(true);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("6Key")){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(true);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("7Key")){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(true);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("8Key")){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(true);
+            cam9.SetActive(false);
+            cam10.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("9Key")){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(true);
+            cam10.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("0Key")){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+            cam9.SetActive(false);
+            cam10.SetActive(true);
         }
     }
 }
