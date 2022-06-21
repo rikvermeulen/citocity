33d5e047f9676d0babdc348f4ace23f0ed090d64
koenbenne <koen.benne@gmail.com>
2022-06-21 14:51:31 +0200

    add final stage scene

--- Assets/Scripts/Core/GoalManager.cs
+++ Assets/Scripts/Core/GoalManager.cs
@@ -11,7 +11,7 @@ public class GoalManager : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-        {         
+        {
             cam.MoveToNewRoom(nextRoom);
         }
     }
