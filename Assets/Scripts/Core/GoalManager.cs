5209d56acab40488478a6ee3fd365a709c80bc2c
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2022-06-21 13:47:14 +0200

    added room functionality to switch

---
+++ Assets/Scripts/Core/GoalManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour
+{
+    [SerializeField] private Transform previousRoom;
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private RoomController cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {         
+            cam.MoveToNewRoom(nextRoom);
+        }
+    }
+}
